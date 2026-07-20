58ef09f33eb890849806b1ab3cacc25c53189877
桃子 <taozi@local>
2026-07-20 16:23:18 +0800

    初始提交：2026简谱动态曲谱播放器（静态站点）

---
+++ scores_data.js
@@ -0,0 +1,1363 @@
+// 曲谱数据。pages 为纯图片文件名（如 img0001.png）；实际地址 = app.js 中 IMG_BASE_URL 前缀 + 此文件名。
+const SCORES = [
+  {
+    "name": "eutopia",
+    "pages": [
+      "img0001.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "jinglebells",
+    "pages": [
+      "img0002.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "lettinggo",
+    "pages": [
+      "img0003.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "reason",
+    "pages": [
+      "img0004.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "一剪梅",
+    "pages": [
+      "img0005.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "一次就好",
+    "pages": [
+      "img0006.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "一直很安静",
+    "pages": [
+      "img0007.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "一程山路",
+    "pages": [
+      "img0008.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "一笑江湖",
+    "pages": [
+      "img0009.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "一路向北",
+    "pages": [
+      "img0010.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "一路生花",
+    "pages": [
+      "img0011.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "不染",
+    "pages": [
+      "img0012.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "不要忘记我爱你",
+    "pages": [
+      "img0013.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "不该",
+    "pages": [
+      "img0014.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "为你我受冷风吹",
+    "pages": [
+      "img0015.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "九张机",
+    "pages": [
+      "img0016.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "云与海",
+    "pages": [
+      "img0017.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "云裳羽衣曲",
+    "pages": [
+      "img0018.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "人世间",
+    "pages": [
+      "img0019.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "他不懂",
+    "pages": [
+      "img0020.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "他不爱我",
+    "pages": [
+      "img0021.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "你",
+    "pages": [
+      "img0022.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "你好不好",
+    "pages": [
+      "img0023.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "倒数",
+    "pages": [
+      "img0024.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "假如爱有天意",
+    "pages": [
+      "img0025.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "偏爱",
+    "pages": [
+      "img0026.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "兰亭序",
+    "pages": [
+      "img0027.JPG",
+      "img0028.JPG",
+      "img0029.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "兰亭序洞洞谱",
+    "pages": [
+      "img0030.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "关键词",
+    "pages": [
+      "img0031.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "冬眠",
+    "pages": [
+      "img0032.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "凉凉",
+    "pages": [
+      "img0033.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "匆匆那年",
+    "pages": [
+      "img0034.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "化蝶",
+    "pages": [
+      "img0035.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "医生",
+    "pages": [
+      "img0036.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "千古",
+    "pages": [
+      "img0037.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "半壶纱",
+    "pages": [
+      "img0038.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "双面燕洵",
+    "pages": [
+      "img0039.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "反方向的钟",
+    "pages": [
+      "img0040.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "发如雪",
+    "pages": [
+      "img0041.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "只若初见",
+    "pages": [
+      "img0042.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "可不可以",
+    "pages": [
+      "img0043.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "可念不可说",
+    "pages": [
+      "img0044.GIF"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "后继者",
+    "pages": [
+      "img0045.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "吹梦到西洲",
+    "pages": [
+      "img0046.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "命运",
+    "pages": [
+      "img0047.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "喜欢两个人",
+    "pages": [
+      "img0048.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "噢！乖",
+    "pages": [
+      "img0049.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "回忆的沙漏",
+    "pages": [
+      "img0050.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "壁上观",
+    "pages": [
+      "img0051.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "多情种",
+    "pages": [
+      "img0052.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "多远都要在一起",
+    "pages": [
+      "img0053.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "夜曲",
+    "pages": [
+      "img0054.JPG",
+      "img0055.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "夜的第七章",
+    "pages": [
+      "img0056.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "大鱼",
+    "pages": [
+      "img0057.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "天涯过客",
+    "pages": [
+      "img0058.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "天空之城",
+    "pages": [
+      "img0059.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "天若有情",
+    "pages": [
+      "img0060.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "天龙八部之宿敌",
+    "pages": [
+      "img0061.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "失落沙洲",
+    "pages": [
+      "img0062.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "女人花",
+    "pages": [
+      "img0063.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "女儿情",
+    "pages": [
+      "img0064.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "她说",
+    "pages": [
+      "img0065.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "如愿",
+    "pages": [
+      "img0066.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "如果可以",
+    "pages": [
+      "img0067.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "如果我变成回忆",
+    "pages": [
+      "img0068.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "如果有你在",
+    "pages": [
+      "img0069.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "孤雏",
+    "pages": [
+      "img0070.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "容易受伤的女人",
+    "pages": [
+      "img0071.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "寂寞在唱歌",
+    "pages": [
+      "img0072.GIF"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "小宇",
+    "pages": [
+      "img0073.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "山水之间",
+    "pages": [
+      "img0074.JPG",
+      "img0075.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "岁月神偷",
+    "pages": [
+      "img0076.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "左手指月",
+    "pages": [
+      "img0077.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "市集",
+    "pages": [
+      "img0078.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "年轮",
+    "pages": [
+      "img0079.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "幻听",
+    "pages": [
+      "img0080.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "康康舞曲",
+    "pages": [
+      "img0081.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "开始懂了",
+    "pages": [
+      "img0082.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "异客",
+    "pages": [
+      "img0083.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "彩云之南",
+    "pages": [
+      "img0084.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "心碎的声音",
+    "pages": [
+      "img0085.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "情歌",
+    "pages": [
+      "img0086.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "想你的365天",
+    "pages": [
+      "img0087.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "我不难过",
+    "pages": [
+      "img0088.GIF",
+      "img0089.GIF"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "我会好好的",
+    "pages": [
+      "img0090.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "我好想你",
+    "pages": [
+      "img0091.JPG",
+      "img0092.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "我心永恒",
+    "pages": [
+      "img0093.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "我的一个道姑朋友",
+    "pages": [
+      "img0094.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "我真的受伤了",
+    "pages": [
+      "img0095.GIF"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "我走后",
+    "pages": [
+      "img0096.JPG",
+      "img0097.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "扬鞭催马",
+    "pages": [
+      "img0098.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "护花使者",
+    "pages": [
+      "img0099.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "故乡的原风景",
+    "pages": [
+      "img0100.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "断了的弦",
+    "pages": [
+      "img0101.GIF"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "新不了情",
+    "pages": [
+      "img0102.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "新鸳鸯蝴蝶梦",
+    "pages": [
+      "img0103.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "无可救药",
+    "pages": [
+      "img0104.GIF"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "时间煮雨",
+    "pages": [
+      "img0105.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "明天你好",
+    "pages": [
+      "img0106.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "晚安",
+    "pages": [
+      "img0107.GIF"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "晴天",
+    "pages": [
+      "img0108.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "曲终人散",
+    "pages": [
+      "img0109.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "曹操",
+    "pages": [
+      "img0110.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "最后一页",
+    "pages": [
+      "img0111.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "最长的电影",
+    "pages": [
+      "img0112.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "月牙湾",
+    "pages": [
+      "img0113.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "有一种悲伤",
+    "pages": [
+      "img0114.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "朋友的酒",
+    "pages": [
+      "img0115.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "枉凝眉",
+    "pages": [
+      "img0116.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "枫",
+    "pages": [
+      "img0117.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "桃花诺",
+    "pages": [
+      "img0118.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "梁祝",
+    "pages": [
+      "img0119.JPG",
+      "img0120.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "梅香如故",
+    "pages": [
+      "img0121.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "梦一场",
+    "pages": [
+      "img0122.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "梦中的婚礼",
+    "pages": [
+      "img0123.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "森林狂想曲",
+    "pages": [
+      "img0124.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "欢乐斗地主",
+    "pages": [
+      "img0125.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "残酷月光",
+    "pages": [
+      "img0126.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "水星记",
+    "pages": [
+      "img0127.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "水调歌头",
+    "pages": [
+      "img0128.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "水边的阿狄丽娜",
+    "pages": [
+      "img0129.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "沈园外",
+    "pages": [
+      "img0130.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "泡沫",
+    "pages": [
+      "img0131.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "浮光",
+    "pages": [
+      "img0132.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "海阔天空",
+    "pages": [
+      "img0133.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "消散对白",
+    "pages": [
+      "img0134.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "清明雨上",
+    "pages": [
+      "img0135.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "溯洞洞谱",
+    "pages": [
+      "img0136.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "灰色头像",
+    "pages": [
+      "img0137.JPG",
+      "img0138.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "烟花易冷",
+    "pages": [
+      "img0139.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "爱似水仙",
+    "pages": [
+      "img0140.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "爱殇",
+    "pages": [
+      "img0141.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "牵丝戏",
+    "pages": [
+      "img0142.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "琵琶行",
+    "pages": [
+      "img0143.JPG",
+      "img0144.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "生日快乐",
+    "pages": [
+      "img0145.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "用心良苦",
+    "pages": [
+      "img0146.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "用情",
+    "pages": [
+      "img0147.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "甩葱歌",
+    "pages": [
+      "img0148.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "画心",
+    "pages": [
+      "img0149.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "百鸟朝凤",
+    "pages": [
+      "img0150.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "盗墓笔记十年人生",
+    "pages": [
+      "img0151.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "相思遥",
+    "pages": [
+      "img0152.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "相见恨晚",
+    "pages": [
+      "img0153.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "相许",
+    "pages": [
+      "img0154.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "知否知否",
+    "pages": [
+      "img0155.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "离别开出花",
+    "pages": [
+      "img0156.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "穿越时空的思念筒2",
+    "pages": [
+      "img0157.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "穿越时空的思念筒5",
+    "pages": [
+      "img0158.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "笑红尘",
+    "pages": [
+      "img0159.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "第一次爱的人",
+    "pages": [
+      "img0160.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "红颜劫",
+    "pages": [
+      "img0161.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "红颜旧",
+    "pages": [
+      "img0162.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "缘分一道桥",
+    "pages": [
+      "img0163.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "美丽的神话",
+    "pages": [
+      "img0164.JPG",
+      "img0165.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "老男孩",
+    "pages": [
+      "img0166.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "背影",
+    "pages": [
+      "img0167.JPG",
+      "img0168.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "舍得",
+    "pages": [
+      "img0169.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "花海",
+    "pages": [
+      "img0170.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "苏公堤",
+    "pages": [
+      "img0171.jpg"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "荒",
+    "pages": [
+      "img0172.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "莫失莫忘",
+    "pages": [
+      "img0173.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "莫问归期",
+    "pages": [
+      "img0174.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "落了白",
+    "pages": [
+      "img0175.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "落空",
+    "pages": [
+      "img0176.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "蒲公英的约定",
+    "pages": [
+      "img0177.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "蜜雪冰城",
+    "pages": [
+      "img0178.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "蝴蝶泉边",
+    "pages": [
+      "img0179.JPG",
+      "img0180.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "街角的祝福",
+    "pages": [
+      "img0181.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "西海情歌",
+    "pages": [
+      "img0182.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "认真的雪",
+    "pages": [
+      "img0183.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "诀别书",
+    "pages": [
+      "img0184.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "说了再见",
+    "pages": [
+      "img0185.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "谁",
+    "pages": [
+      "img0186.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "赤伶",
+    "pages": [
+      "img0187.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "走不出的回忆",
+    "pages": [
+      "img0188.JPG",
+      "img0189.JPG"
+    ],
+    "type": "multi"
+  },
+  {
+    "name": "跳楼机",
+    "pages": [
+      "img0190.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "过客",
+    "pages": [
+      "img0191.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "这世界那么多人",
+    "pages": [
+      "img0192.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "这样爱了",
+    "pages": [
+      "img0193.GIF"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "退后",
+    "pages": [
+      "img0194.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "遇见",
+    "pages": [
+      "img0195.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "遗憾",
+    "pages": [
+      "img0196.PNG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "醉清波",
+    "pages": [
+      "img0197.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "错的人",
+    "pages": [
+      "img0198.png"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "雨一直下",
+    "pages": [
+      "img0199.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "雨下一整晚间奏",
+    "pages": [
+      "img0200.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "雨爱",
+    "pages": [
+      "img0201.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "雪落下的声音",
+    "pages": [
+      "img0202.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "青玉案元夕",
+    "pages": [
+      "img0203.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "青花",
+    "pages": [
+      "img0204.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "青花瓷",
+    "pages": [
+      "img0205.JPG"
+    ],
+    "type": "single"
+  },
+  {
+    "name": "默",
+    "pages": [
+      "img0206.JPG"
+    ],
+    "type": "single"
+  }
+];
+
+module.exports = SCORES;
